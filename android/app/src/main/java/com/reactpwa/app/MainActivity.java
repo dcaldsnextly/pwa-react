798b4f389166511b1bbcd86cdf7396f96b79a121
Danilo Caldas <danilo.caldas@leanonsystems.com>
2023-03-23 17:29:21 -0300

    Added capacitor

---
+++ android/app/src/main/java/com/reactpwa/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.reactpwa.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.reactpwa.app',
+  appName: 'React PWA',
+  webDir: 'build',
+  bundledWebRuntime: true
+};
+
+export default config;
